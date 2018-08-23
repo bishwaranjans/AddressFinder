6be6218155b1b0c3f92d5ddd4bc2597a0f3b87df
Bishwaranjan Sandhu <BSandhu@slb.com>
2018-08-23 21:47:46 +0200

    Added Unit tests to project

---
+++ AddressFinder.Tests/CanadianAddressFinderTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AddressFinder.Domain.SeedWork;
+using AddressFinder.Infrastructure.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddressFinder.Tests
+{
+    [TestClass]
+    public class CanadianAddressFinderTest
+    {
+        private readonly CanadianAddressFinder _canadianAddressFinder;
+
+        public CanadianAddressFinderTest()
+        {
+            ILoggerManager logger = new LoggerManager();
+            _canadianAddressFinder = new CanadianAddressFinder(logger);
+        }
+
+        [TestMethod]
+        public void GetAddresseeNameFromMultilineStringTest()
+        {
+            var stringAddress = @"BISHWARANJAN SANDHU
+                             Marketing Department
+                             10-123 1/2 MAIN ST SE
+                             MONTREAL QC   H3Z 2Y7";
+
+            var addressDetails = _canadianAddressFinder.GetAddressFromMultilineString(stringAddress);
+
+            Assert.AreEqual("BISHWARANJAN SANDHU", addressDetails["Addressee"]);
+        }
+
+        [TestMethod]
+        public void GetPostalCodeFromMultilineStringTest()
+        {
+            var stringAddress = @"BISHWARANJAN SANDHU
+                             Marketing Department
+                             10-123 1/2 MAIN ST SE
+                             MONTREAL QC   H3Z 2Y7";
+
+            var addressDetails = _canadianAddressFinder.GetAddressFromMultilineString(stringAddress);
+
+            Assert.AreEqual("H3Z 2Y7", addressDetails["PostalCode"]);
+        }
+
+        [TestMethod]
+        public void GetMunicipalityFromMultilineStringTest()
+        {
+            var stringAddress = @"BISHWARANJAN SANDHU
+                             Marketing Department
+                             10-123 1/2 MAIN ST SE
+                             MONTREAL QC   H3Z 2Y7";
+
+            var addressDetails = _canadianAddressFinder.GetAddressFromMultilineString(stringAddress);
+
+            Assert.AreEqual("MONTREAL", addressDetails["Municipality"]);
+        }
+    }
+}
